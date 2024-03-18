5cbe5eb89dccd67118f98f0d6326bd253f30fa08
Abdulla-Eshquvvatov <abdullaeshquvvatov22@gmail.com>
2024-03-18 10:46:53 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let month = prompt("oy ramini kiring");
+switch (month) {
+  case "1":
+    console.log("yanvar");
+    break;
+  case "2":
+    console.log("Fevral");
+    break;
+  case "3":
+    console.log("Mart");
+    break;
+  case "4":
+    console.log("aprel");
+    break;
+  case "5":
+    console.log("May");
+    break;
+  case "6":
+    console.log("Iyun");
+    break;
+  case "7":
+    console.log("Iyul");
+    break;
+  case "8":
+    console.log("Avgust");
+    break;
+  case "9":
+    console.log("Senteabr");
+    break;
+  case "10":
+    console.log("Oktabr");
+    break;
+  case "11":
+    console.log("Noyabr");
+    break;
+  case "12":
+    console.log("Dekabr");
+    break;
+  default:
+    console.log("Bunday oy mavjud emas");
+}
